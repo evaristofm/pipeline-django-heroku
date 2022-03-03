01d282da2e2f3651cc077c5e6b937774d131ff4b
evaristofm <evaristofm@gmail.com>
2022-03-02 23:42:11 -0300

    Remodelando o app

--- api_basic/admin.py
+++ api_basic/admin.py
@@ -1,9 +1,1 @@
 from django.contrib import admin
-from .models import Article
-
-
-@admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
-    pass
-
-
--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,23 +1,10 @@
 from django.urls import path, include
-from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework.routers import DefaultRouter
 
 from . import views
 
-router = DefaultRouter()
-router.register('articles', views.ArticleViewSet, basename='articles')
-router.register('users', views.UserViewSet, basename='articles')
-
-
-
 
 urlpatterns = [
-    path('api/', include(router.urls)),
     path('', views.index, name='index')
-   # path('articles/', views.ArticleApiView.as_view()),
-   # path('articles/', views.GenericApiView.as_view()),
-   # path('articles/<int:id>/', views.GenericApiView.as_view()),
-   # path('articles/<int:id>/', views.ArticleDetail.as_view()),
 ]
 
 
